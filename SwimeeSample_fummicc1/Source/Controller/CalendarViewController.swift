86adcb01ce15cab492dfe417c9945e6d39c3a5b4
fummicc1-sub <fummiya989@gmail.com>
2018-10-28 18:54:03 +0900

    OKまる

--- SwimeeSample_fummicc1/Source/Controller/CalendarViewController.swift
+++ SwimeeSample_fummicc1/Source/Controller/CalendarViewController.swift
@@ -14,15 +14,13 @@ class CalendarViewController: UIViewController {
 
     var realm: Realm! //利便性のためにパブリックな変数で宣言しておく。
     
+    var diaries: Results<Diary>! //日記のデータを保存しておくためのもの。
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // DBのファイルの場所
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
-        
-        // realmをインスタンス化
-        
+        print(Realm.Configuration.defaultConfiguration.fileURL!) // DBのファイルの場所
+        realm = try! Realm() // realmをインスタンス化
     }
     
 }
@@ -31,10 +29,17 @@ extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegate {
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         
-    
-        // 画面遷移を行う。
-        let diaryVC = UIStoryboard(name: "MakeDiaryViewController", bundle: nil).instantiateInitialViewController() as! MakeDiaryViewController
-        diaryVC.date = date
-        present(diaryVC, animated: true, completion: nil)
+        if let diary = realm.objects(Diary.self).filter("postDate== %@", date).first {
+            let showDiaryVC = UIStoryboard(name: "ShowDiaryViewController", bundle: nil).instantiateInitialViewController() as! ShowDiaryViewController
+            showDiaryVC.diary = diary
+            present(showDiaryVC, animated: true, completion: nil)
+        }else {
+            // 画面遷移を行う。
+            let makeDiaryVC = UIStoryboard(name: "MakeDiaryViewController", bundle: nil).instantiateInitialViewController() as! MakeDiaryViewController
+            makeDiaryVC.date = date
+            present(makeDiaryVC, animated: true, completion: nil)
+        }
+        
+        
     }
 }
