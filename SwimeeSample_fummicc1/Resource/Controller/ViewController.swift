88a8d55355c3353516e489c400c867920acbd7e6
fummicc1-sub <fummiya989@gmail.com>
2018-10-28 12:36:34 +0900

    Initial Commit

---
+++ SwimeeSample_fummicc1/Resource/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwimeeSample_fummicc1
+//
+//  Created by Fumiya Tanaka on 2018/10/28.
+//  Copyright © 2018 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
